14a479c1a1cddad8f97eeb0bfc10fbf45d2bd3ab
Geert Barentsen <geert@barentsen.be>
2015-06-30 11:15:12 -0700

    Python 2 utf-8 handling fix

--- kpub/kpub.py
+++ kpub/kpub.py
@@ -1,10 +1,11 @@
 """Build and maintain a database of Kepler/K2 publications.
 """
-from __future__ import print_function, division
+from __future__ import print_function, division, unicode_literals
 
 # Standard library
 import os
 import re
+import sys
 import json
 import datetime
 import argparse
@@ -13,6 +14,7 @@ import sqlite3 as sql
 # External dependencies
 import ads
 import jinja2
+from six.moves import input  # needed to support Python 2
 from astropy import log
 from astropy.utils.console import ProgressBar
 
@@ -162,6 +164,10 @@ class PublicationDB(object):
                 else:
                     self.add(article, **kwargs)
 
+    def delete_by_bibcode(self, bibcode):
+        self.con.execute("DELETE FROM pubs WHERE bibcode = ?;", [bibcode])
+        self.con.commit()
+
     def __contains__(self, article):
         count = self.con.execute("SELECT COUNT(*) FROM pubs WHERE id = ?;",
                                  [article.id]).fetchone()[0]
@@ -209,7 +215,11 @@ class PublicationDB(object):
         templatedir = os.path.join(PACKAGEDIR, 'templates')
         env = jinja2.Environment(loader=jinja2.FileSystemLoader(templatedir))
         template = env.get_template('template.md')
-        return template.render(articles=articles)
+        markdown = template.render(articles=articles)
+        if sys.version_info >= (3, 0):
+            return markdown  # Python 3
+        else:
+            return markdown.encode("utf-8")  # Python 2
 
     def update(self, month=None, exclude=['keplerian', 'johannes']):
         """Query ADS for new publications.
@@ -226,7 +236,8 @@ class PublicationDB(object):
         if month is None:
             month = datetime.datetime.now().strftime("%Y-%m")
 
-        qry = ads.query('abs:"Kepler" OR abs:"K2"',
+        log.info("Querying NASA ADS for {}.".format(month))
+        qry = ads.query('abs:"Kepler" OR abs:"K2" OR title:"Kepler" OR title:"K2"',
                         dates=month,
                         rows='all',
                         database='astronomy')  # ,property='refereed')
@@ -253,84 +264,10 @@ class PublicationDB(object):
                 ignore = True
 
             if not ignore:  # Propose to the user
-                statusmsg = '(Reviewing article {} out of {}.)\n\n'.format(idx, len(articles))
+                statusmsg = '(Reviewing article {} out of {}.)\n\n'.format(
+                                idx, len(articles))
                 self.add_interactively(article, statusmsg=statusmsg)
         log.info('Finished reviewing all articles for {}.'.format(month))
-
-
-def kpub_export(args=None):
-    """Export the bibcodes and classifications in CSV format."""
-    parser = argparse.ArgumentParser(
-        description="Export the Kepler/K2 publication list in CSV format.")
-    parser.add_argument('-f', metavar='dbfile',
-                        type=str, default=DEFAULT_DB,
-                        help="Location of the Kepler/K2 publication list db. "
-                             "Defaults to '~/.kpub.db'.")
-    args = parser.parse_args(args)
-
-    db = PublicationDB(args.f)
-    cur = db.con.execute("SELECT bibcode, mission, science "
-                         "FROM pubs ORDER BY bibcode;")
-    for row in cur.fetchall():
-        print('{0},{1},{2}'.format(row[0], row[1], row[2]))
-
-
-def kpub_import(args=None):
-    """Import publications from a csv file.
-
-    The csv file must contain entries of the form "bibcode,mission,science".
-    The actual metadata of each publication will be grabbed using the ADS API,
-    hence this routine may take 10-20 minutes to complete.
-    """
-    parser = argparse.ArgumentParser(
-        description="Batch-import papers into the Kepler/K2 publication list "
-                    "from a CSV file. The CSV file must have three columns "
-                    "(bibcode,mission,science) separated by commas. "
-                    "For example: '2004ApJ...610.1199G,kepler,astrophysics'.")
-    parser.add_argument('-f', metavar='dbfile',
-                        type=str, default=DEFAULT_DB,
-                        help="Location of the Kepler/K2 publication list db. "
-                             "Defaults to '~/.kpub.db'.")
-    parser.add_argument('csvfile',
-                        help="Filename of the csv file to ingest.")
-    args = parser.parse_args(args)
-
-    db = PublicationDB(args.f)
-    for line in ProgressBar(open(args.csvfile, 'r').readlines()):
-        col = line.split(',')  # Naive csv parsing
-        db.add_by_bibcode(col[0], mission=col[1], science=col[2].strip())
-
-
-def kpub_add(args=None):
-    """Add a publication with a known ADS bibcode."""
-    parser = argparse.ArgumentParser(
-        description="Add a paper to the Kepler/K2 publication list.")
-    parser.add_argument('-f', metavar='dbfile',
-                        type=str, default=DEFAULT_DB,
-                        help="Location of the Kepler/K2 publication list db."
-                             "Defaults to '~/.kpub.db'.")
-    parser.add_argument('bibcode', nargs='+',
-                        help='ADS bibcode that identifies the publication.')
-    args = parser.parse_args(args)
-
-    db = PublicationDB(args.f)
-    for bibcode in args.bibcode:
-        db.add_by_bibcode(bibcode, interactive=True)
-
-
-def kpub_update(args=None):
-    """Interactively query ADS for new publications."""
-    parser = argparse.ArgumentParser(
-        description="Interactively query ADS for new publications.")
-    parser.add_argument('-f', metavar='dbfile',
-                        type=str, default=DEFAULT_DB,
-                        help="Location of the Kepler/K2 publication list db."
-                             "Defaults to '~/.kpub.db'.")
-    parser.add_argument('month', nargs='?', default=None,
-                        help='Month to query, e.g. 2015-06.')
-    args = parser.parse_args(args)
-
-    PublicationDB(args.f).update(month=args.month)
 
 
 def kpub(args=None):
@@ -371,7 +308,101 @@ def kpub(args=None):
     output = db.to_markdown(group_by_month=args.month,
                             mission=mission,
                             science=science)
+    from signal import signal, SIGPIPE, SIG_DFL
+    signal(SIGPIPE, SIG_DFL)
     print(output)
+
+
+def kpub_update(args=None):
+    """Interactively query ADS for new publications."""
+    parser = argparse.ArgumentParser(
+        description="Interactively query ADS for new publications.")
+    parser.add_argument('-f', metavar='dbfile',
+                        type=str, default=DEFAULT_DB,
+                        help="Location of the Kepler/K2 publication list db."
+                             "Defaults to '~/.kpub.db'.")
+    parser.add_argument('month', nargs='?', default=None,
+                        help='Month to query, e.g. 2015-06.')
+    args = parser.parse_args(args)
+
+    PublicationDB(args.f).update(month=args.month)
+
+
+def kpub_add(args=None):
+    """Add a publication with a known ADS bibcode."""
+    parser = argparse.ArgumentParser(
+        description="Add a paper to the Kepler/K2 publication list.")
+    parser.add_argument('-f', metavar='dbfile',
+                        type=str, default=DEFAULT_DB,
+                        help="Location of the Kepler/K2 publication list db."
+                             "Defaults to '~/.kpub.db'.")
+    parser.add_argument('bibcode', nargs='+',
+                        help='ADS bibcode that identifies the publication.')
+    args = parser.parse_args(args)
+
+    db = PublicationDB(args.f)
+    for bibcode in args.bibcode:
+        db.add_by_bibcode(bibcode, interactive=True)
+
+
+def kpub_delete(args=None):
+    """Deletes a publication using its ADS bibcode."""
+    parser = argparse.ArgumentParser(
+        description="Deletes a paper from the Kepler/K2 publication list.")
+    parser.add_argument('-f', metavar='dbfile',
+                        type=str, default=DEFAULT_DB,
+                        help="Location of the Kepler/K2 publication list db."
+                             "Defaults to '~/.kpub.db'.")
+    parser.add_argument('bibcode', nargs='+',
+                        help='ADS bibcode that identifies the publication.')
+    args = parser.parse_args(args)
+
+    db = PublicationDB(args.f)
+    for bibcode in args.bibcode:
+        db.delete_by_bibcode(bibcode)
+
+
+def kpub_import(args=None):
+    """Import publications from a csv file.
+
+    The csv file must contain entries of the form "bibcode,mission,science".
+    The actual metadata of each publication will be grabbed using the ADS API,
+    hence this routine may take 10-20 minutes to complete.
+    """
+    parser = argparse.ArgumentParser(
+        description="Batch-import papers into the Kepler/K2 publication list "
+                    "from a CSV file. The CSV file must have three columns "
+                    "(bibcode,mission,science) separated by commas. "
+                    "For example: '2004ApJ...610.1199G,kepler,astrophysics'.")
+    parser.add_argument('-f', metavar='dbfile',
+                        type=str, default=DEFAULT_DB,
+                        help="Location of the Kepler/K2 publication list db. "
+                             "Defaults to '~/.kpub.db'.")
+    parser.add_argument('csvfile',
+                        help="Filename of the csv file to ingest.")
+    args = parser.parse_args(args)
+
+    db = PublicationDB(args.f)
+    for line in ProgressBar(open(args.csvfile, 'r').readlines()):
+        col = line.split(',')  # Naive csv parsing
+        db.add_by_bibcode(col[0], mission=col[1], science=col[2].strip())
+
+
+def kpub_export(args=None):
+    """Export the bibcodes and classifications in CSV format."""
+    parser = argparse.ArgumentParser(
+        description="Export the Kepler/K2 publication list in CSV format.")
+    parser.add_argument('-f', metavar='dbfile',
+                        type=str, default=DEFAULT_DB,
+                        help="Location of the Kepler/K2 publication list db. "
+                             "Defaults to '~/.kpub.db'.")
+    args = parser.parse_args(args)
+
+    db = PublicationDB(args.f)
+    cur = db.con.execute("SELECT bibcode, mission, science "
+                         "FROM pubs ORDER BY bibcode;")
+    for row in cur.fetchall():
+        print('{0},{1},{2}'.format(row[0], row[1], row[2]))
 
 
 if __name__ == '__main__':
