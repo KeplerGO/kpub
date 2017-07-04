a0d557e17f5027c5da03afb38527f4f06c45d923
Geert Barentsen <geert@barentsen.be>
2017-07-03 21:19:27 -0700

    Initial commit of a script to plot the global distribution of K2 authors on a world map

---
+++ scripts/community-world-map/1-get-coordinates.py
@@ -0,0 +1,49 @@
+"""Obtain the affiliation coordinates of the first authors of K2 papers.
+
+Writes a file called `coordinates.csv`.
+"""
+import json
+import nltk
+import time
+from tqdm import tqdm
+import kpub
+
+OUTPUT_FN = 'coordinates.csv'
+
+# Step 1: obtain the first author affiliations from kpub
+locations = []
+db = kpub.PublicationDB()
+all_publications = db.get_all(mission='k2')
+for publication in all_publications:
+    affiliations = publication['aff'][0].split(";")
+    # Use the first three authors
+    for aff in affiliations[0:3]:
+        # Help the geolocator by only passing on the final components of the address
+        aff_suffix = ",".join(aff.split(",")[-2:]).strip()
+        locations.append(aff_suffix)
+unique_locations = nltk.FreqDist(locations)
+print("Found {} unique locations".format(len(unique_locations)))
+
+# Step 2: initialize the Google geolocator
+from geopy.geocoders import GoogleV3
+geolocator = GoogleV3()
+time.sleep(1)
+
+# Step 3: geolocate all affiliations
+out = open(OUTPUT_FN, "w")
+out.write("lon,lat,count,name\n")
+fd_aff = nltk.FreqDist(locations)
+for name, count in tqdm(unique_locations.items()):
+    if name == "-" or name == "":
+        continue
+    try:
+        location = geolocator.geocode(name, timeout=10)
+        outstring = "{},{},{},{}\n".format(location.longitude, location.latitude, count, name.replace(",", ";"))
+        out.write(outstring)
+        out.flush()
+        print("Success: {} = {}".format(name, outstring))
+    except Exception as e:
+        print("Warning: failed to geolocate {} (exception: {})".format(name, e))
+        pass
+    time.sleep(1)
+out.close()
---
+++ scripts/community-world-map/2-plot-map.py
@@ -0,0 +1,75 @@
+"""Plot the K2 community coordinates using basemap.
+"""
+import matplotlib.pyplot as pl
+from mpl_toolkits.basemap import Basemap
+import pandas
+
+df = pandas.read_csv("coordinates.csv")
+
+fig = pl.figure(figsize=(16, 9))
+ax = fig.add_axes([0.05, 0.05, 0.9, 0.9])
+ax.set_facecolor('white')
+m = Basemap(projection='kav7',
+            lon_0=-90, lat_0=0,
+            resolution="l", fix_aspect=False)
+m.drawcountries(color='#7f8c8d', linewidth=1.)
+m.drawstates(color='#bdc3c7', linewidth=0.5)
+m.drawcoastlines(color='#7f8c8d', linewidth=1.)
+m.fillcontinents('#ecf0f1', zorder=0)
+x, y = m(df['lon'].values, df['lat'].values)
+m.scatter(x, y, marker="x", color="red", lw=2.5, s=100, zorder=999)
+pl.savefig("k2-authors-world-map.png")
+pl.savefig("k2-authors-world-map.pdf")
+
+"""
+fig = pl.figure(figsize=(16, 9))
+ax = fig.add_axes([0.05,0.05,0.9,0.9])
+
+m = Basemap(projection='cyl',
+                           llcrnrlon=-134, llcrnrlat=23,
+                           urcrnrlon=-57, urcrnrlat=53,
+                           resolution="l", fix_aspect=False)
+#m.drawcoastlines()
+m.drawcountries(color='#7f8c8d', linewidth=1.)
+m.drawstates(color='#bdc3c7', linewidth=0.5)
+m.drawcoastlines(color='#7f8c8d', linewidth=1.)
+m.fillcontinents('#ecf0f1', zorder=0)
+m.scatter(df['lon'], df['lat'], marker="x", color="red", lw=2.5, s=100, zorder=999)
+#ax.set_title('K2 Author Affiliations')
+#pl.tight_layout()
+#pl.show()
+pl.savefig("k2-authors-usa.png")
+
+
+m = Basemap(projection='cyl',
+                           llcrnrlon=-15, llcrnrlat=35,
+                           urcrnrlon=28, urcrnrlat=62,
+                           resolution="l", fix_aspect=False)
+#m.drawcoastlines()
+m.drawcountries(color='#7f8c8d', linewidth=1.)
+m.drawstates(color='#bdc3c7', linewidth=0.5)
+m.drawcoastlines(color='#7f8c8d', linewidth=1.)
+m.fillcontinents('#ecf0f1', zorder=0)
+m.scatter(df['lon'], df['lat'], marker="x", color="red", lw=2.5, s=100, zorder=999)
+#ax.set_title('K2 Author Affiliations')
+#pl.tight_layout()
+#pl.show()
+pl.savefig("k2-authors-europe.png")
+"""
+
+"""
+fig = pl.figure(figsize=(16, 9))
+ax = fig.add_axes([0.05,0.05,0.9,0.9])
+m = Basemap(projection='cyl',
+                           llcrnrlon=-25, llcrnrlat=-52,
+                           urcrnrlon=180, urcrnrlat=80,
+                           resolution="l", fix_aspect=False)
+m.drawcountries(color='#7f8c8d', linewidth=1.)
+m.drawstates(color='#bdc3c7', linewidth=0.5)
+m.drawcoastlines(color='#7f8c8d', linewidth=1.)
+m.fillcontinents('#ecf0f1', zorder=0)
+m.scatter(df['lon'], df['lat'], marker="x", color="red", lw=2.5, s=100, zorder=999)
+pl.savefig("k2-authors-asia.png")
+"""
+
+pl.close("all")
