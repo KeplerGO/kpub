259f7b579d2c8704faef00d5fb2b0c3f75820cef
Geert Barentsen <geert@barentsen.be>
2017-07-09 15:05:13 -0700

    Clean up script

--- scripts/science-categories/plot-pie-chart.py
+++ scripts/science-categories/plot-pie-chart.py
@@ -1,15 +1,10 @@
+"""Make a pie chart plot showing the distribution of K2 papers by science theme.
+"""
 import pandas as pd
 import matplotlib.pyplot as pl
-#import seaborn as sns
 
 from categorize import CATEGORIES
 
-FLAT = ["#1abc9c", "#2ecc71", "#3498db", "#9b59b6",
-"#f1c40f", "#e67e22", "#e74c3c", 
-"#16a085", "#27ae60", "#2980b9", "#8e44ad",
-         "#f39c12", "#d35400", "#c0392b"]
-
-FLAT = ["#2ecc71", "#27ae60"]
 
 df = pd.read_csv("k2-categories.csv", names=["bibcode", "category"])
 
@@ -27,16 +22,17 @@ counts = df.category.value_counts()
 myorder = ['ed', 'ec', 'ml', 'as', 'ga', 'va', 'wd', 'cl', 'eb', 'yo', 'ac', 'ss', 'ot', 'da']
 assert(len(myorder) == len(counts.index))
 
+#labels = [CATEGORIES[lab] for lab in counts.index]
+#sizes = counts.values
 labels = [CATEGORIES[lab] for lab in myorder]
 sizes = [counts[lab] for lab in myorder]
 
-#sizes = counts.values
-
-fig1, ax = pl.subplots(figsize=(16, 9))
+fig, ax = pl.subplots(figsize=(16, 9))
 ax.pie(sizes, labels=labels, autopct='%1.0f%%', startangle=145,
-       counterclock=False, colors=FLAT,
+       counterclock=False, colors=["#2ecc71", "#27ae60"],
        pctdistance=0.8, labeldistance=1.1,
        textprops={'fontsize': 20, 'weight': 'bold'})
 ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 pl.tight_layout(rect=[0.05, 0.05, 0.95, 0.95])
 pl.savefig("k2-science-piechart.png")
+pl.close("all")
