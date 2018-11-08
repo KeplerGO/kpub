863567cfe8a03871f42c275bb2005c9d07286d62
Geert Barentsen <geert@barentsen.be>
2018-11-08 07:54:52 -0800

    The API now requires a key

--- scripts/community-world-map/1-get-coordinates.py
+++ scripts/community-world-map/1-get-coordinates.py
@@ -2,22 +2,22 @@
 
 Writes a file called `coordinates.csv`.
 """
-import json
 import nltk
 import time
 from tqdm import tqdm
 import kpub
+from geopy.geocoders import GoogleV3
 
 OUTPUT_FN = 'coordinates.csv'
 
 # Step 1: obtain the first author affiliations from kpub
 locations = []
 db = kpub.PublicationDB()
-all_publications = db.get_all(mission='k2')
+all_publications = db.get_all()
 for publication in all_publications:
     affiliations = publication['aff']
     # Use the first three authors
-    for aff in affiliations: #[0:3]:
+    for aff in affiliations:
         # Help the geolocator by only passing on the final components of the address
         aff_suffix = ",".join(aff.split(";")[-1].split(",")[-2:]).strip(" ;,-")
         locations.append(aff_suffix)
@@ -26,9 +26,9 @@ print("Found {} unique locations".format(len(unique_locations)))
 
 
 # Step 2: initialize the Google geolocator
-from geopy.geocoders import GoogleV3
-geolocator = GoogleV3()
-time.sleep(1)
+from config import API_KEY
+geolocator = GoogleV3(api_key=API_KEY)
+time.sleep(2)
 
 # Step 3: geolocate all affiliations
 out = open(OUTPUT_FN, "w")
@@ -46,5 +46,5 @@ for name, count in tqdm(unique_locations.items()):
     except Exception as e:
         print("Warning: failed to geolocate {} (exception: {})".format(name, e))
         pass
-    time.sleep(1)
+    time.sleep(0.3)
 out.close()
