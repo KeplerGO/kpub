0bb320a641f3acb9616843a8b54c8d7ea09cbeb7
Geert Barentsen <geert@barentsen.be>
2019-08-21 16:54:41 -0700

    Update K2 pie chart

--- scripts/science-categories/plot-pie-chart.py
+++ scripts/science-categories/plot-pie-chart.py
@@ -23,7 +23,8 @@ df.loc[df.category == 'cv', 'category'] = 'wd'
 #df.loc[df.category == 'ga', 'category'] = 'as'
 
 counts = df.category.value_counts()
-myorder = ['ed', 'ec', 'sn', 'ga', 'ml', 'ss', 'ro', 'ot', 'yo', 'va', 'ac', 'cl', 'eb', 'as', 'wd', 'da']
+myorder = ['ed', 'ec', 'sn', 'ml', 'ss', 'ga', 'ro', 'ot', 'yo', 'va', 'eb',
+           'ac', 'cl', 'wd', 'as', 'da']
 #assert(len(myorder) == len(counts.index))
 
 #labels = [CATEGORIES[lab] for lab in counts.index]
@@ -31,11 +32,11 @@ myorder = ['ed', 'ec', 'sn', 'ga', 'ml', 'ss', 'ro', 'ot', 'yo', 'va', 'ac', 'cl
 labels = [CATEGORIES[lab] for lab in myorder]
 sizes = [counts[lab] for lab in myorder]
 
-fig, ax = pl.subplots(figsize=(20, 9))
+fig, ax = pl.subplots(figsize=(16, 9))
 ax.pie(sizes, labels=labels, autopct='%1.0f%%', startangle=158,
        counterclock=False, colors=["#2ecc71", "#27ae60"],
        pctdistance=0.8, labeldistance=1.07,
-       textprops={'fontsize': 24, 'weight': 'normal'})
+       textprops={'fontsize': 20, 'weight': 'normal'})
 ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 ttl = pl.title("K2 Mission Papers by Science Topic", fontsize=36)
 ttl.set_position([.5, 1.05])
